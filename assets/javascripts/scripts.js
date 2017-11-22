1066027d55406d7c907a8be5e716b76a72c7da49
Chad Danford <chad.m.danford@gmail.com>
2017-11-22 17:14:28 +0100

    production server ready before thanksgiving holiday

---
+++ assets/javascripts/scripts.js
@@ -0,0 +1,39 @@
+(function ($, root, undefined) {
+
+	$(function () {
+		'use strict';
+
+		// DOM ready, take it away
+
+		var owl = $('.owl-carousel');
+		owl.owlCarousel({
+		    items:1,
+		    loop:true,
+		    margin:10,
+		    autoplay:true,
+				animateOut: 'fadeOut',
+		    autoplayTimeout:4000,
+		    autoplayHoverPause:true,
+				dots: false,
+		});
+
+		var prev = 0;
+		var $window = $(window);
+		var logolong = $('.logo-long');
+		var logoblock = $('.logo-block');
+
+		$window.on('scroll', function(){
+  		var scrollTop = $window.scrollTop();
+  		logolong.toggleClass('popdown', scrollTop > prev);
+  		logoblock.toggleClass('popup', scrollTop > prev);
+  		prev = scrollTop;
+		});
+
+	});
+
+
+
+
+})(jQuery, this);
+
+
